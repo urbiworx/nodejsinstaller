4cbe8243204771b0d07bb6f73cd5f17ae838da9e
Urbiworx <support@urbiworx.de>
2020-02-29 22:10:59 +0100

    Improve stability and security

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@ apt-get update && apt-get dist-upgrade -y
 apt-get install -y nodejs
 apt-get install -y npm
 ln -s "$(which nodejs)" /usr/bin/node
-npm install -g nodemon
+npm install -g forever
+adduser nodeuser$1
 mkdir /usr/local/nodejs$1
 rm /usr/local/nodejs$1/server.js
 touch /usr/local/nodejs$1/server.js
@@ -16,6 +17,7 @@ echo "[Service]" >> /lib/systemd/system/nodejs$1.service
 echo "Type=forking" >> /lib/systemd/system/nodejs$1.service
 echo "Environment=HOME=/root" >> /lib/systemd/system/nodejs$1.service
 echo "ExecStart=/usr/bin/nodeservice$1.sh" >> /lib/systemd/system/nodejs$1.service
+echo "User=nodeuser$1" >> /lib/systemd/system/nodejs$1.service
 echo "" >> /lib/systemd/system/nodejs$1.service
 echo "[Install]" >> /lib/systemd/system/nodejs$1.service
 echo "WantedBy=multi-user.target" >> /lib/systemd/system/nodejs$1.service
@@ -23,7 +25,9 @@ rm /usr/bin/nodeservice$1.sh
 touch /usr/bin/nodeservice$1.sh
 echo "#!/bin/sh" >> /usr/bin/nodeservice$1.sh
 echo "cd /usr/local/nodejs$1" >> /usr/bin/nodeservice$1.sh
-echo "nodemon /usr/local/nodejs$1/server.js --watch /usr/local/nodejs$1 >> /var/log/nodejs$1.log 2>&1 &" >> /usr/bin/nodeservice$1.sh
+echo "forever --watchDirectory /usr/local/nodejs$1 --workingDir /usr/local/nodejs$1 /usr/local/nodejs$1/server.js >> /var/log/nodejs$1.log 2>&1 &" >> /usr/bin/nodeservice$1.sh
 chmod 0777 /usr/bin/nodeservice$1.sh
+chown -c nodeuser$1 /usr/bin/nodeservice$1.sh
+chmod -R 700 /usr/local/nodejs$1
 systemctl enable nodejs$1.service
 systemctl start nodejs$1.service
